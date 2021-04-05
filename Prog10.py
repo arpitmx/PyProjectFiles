fefd9c26efdc9f225d475d0bf6f6b4f769893a51
Arpit Maurya <59350776+arpitmaurya@users.noreply.github.com>
2021-04-05 13:11:33 +0530

    Update Prog10.py

--- Prog10.py
+++ Prog10.py
@@ -1,60 +1,46 @@
 import csv
+def create():
+    f=open('myCSV.csv','w')
+    print()
+    print('File Created')
+    print()
+    f.close()
 
-def creatfile(name):
-    open(name,'x')
-    print("File Created! :",name)
+def add():
+    f=open('myCSV.csv','a')
+    
+    name=input("Enter Name: ")
+    no=int(input("Enter Mobile No.: "))
+    email=input("Enter Email: ")
+    add=input("Enter Address: ")
+    print()
+    print("Done")
+    print()
+    friend=[name,no,email,add]
+    fwriter = csv.writer(f)
+    fwriter.writerow(friend)
+    f.close()
 
-def addfriend(name):
-    with open(name,'a') as file:
-        cfile = csv.writer(file)
-
-        fname = input("Friend's name :")
-        pno = input("Phone number :")
-        email = input("EMail id :")
-        adrs = input("Address :")
-
-        row = [fname,pno,email,adrs]
-
-        cfile.writerow(row)
-        print("Friend Added!")
+def search():
+    friend=input("Enter Name: ")
+    f=open("myCSV.csv",'r')
+    freader=csv.reader(f)
+    for i in freader:
+        if i[0]==friend:
+            print('Found!')
+            print(i) 
+            break
+    
         
-def searchfriend(name):
+    f.close()
 
-    with open(name,'r') as file:
-        cfile = csv.DictReader(file)
-        fname = input("Enter Name of your Friend to search :")
-        count= 0
-        for row in cfile:
-            d = dict(row)
-            if (fname== d.get("Name")):
-                count +=1
-                print('A Friend Found :\n Details -> ', d,"\n---------------------")
-
-        print("Total Matches :",count)            
-
-
-
-def menu():
-    print("1.Create File \n2.Add Friend\n3.Search Friend\n4.Quit")
-    inp = int(input("Choose(1,2,3,4):"))
-    
-    if (inp==1):
-        name = input("Enter name of file :")
-        creatfile(name)
-    elif (inp==2):
-        name = input("Enter name of file :")
-        addfriend(name)
-    elif(inp==3):
-        name = input("Enter name of file :")
-        searchfriend(name)
-    elif(inp==4):
-        quit()        
-
-    
-
-    
-
-menu()
-
-
-
+while True:
+    ch=int(input("Enter choice: 1. Create File\n 2. Add Friend\n 3. Search Friend\n 4.Quit\n"))
+    if ch==1:
+        create()
+    elif ch==2:
+        add()
+    elif ch==3:
+        search()
+    else:
+        break
