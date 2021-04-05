8f7ac4229f19d6dc6a951daae3641e47021eed75
Arpit Maurya <59350776+arpitmaurya@users.noreply.github.com>
2021-04-05 13:27:07 +0530

    Update Prog9.py

--- Prog9.py
+++ Prog9.py
@@ -1,57 +1,60 @@
+import pickle
 print("==========================>\n\n1.Create File \n2.Add appilicant \n3.Search Applicant on the basis of qualification\n4.Modify Appilcant \n5.Quit")
 
 inp= int(input("Enter your Choice :"))
 
 if (inp ==1):
-    name = input("Enter file name :")
-    file = open(name ,'x')
+    file = open('data.DAT' ,'wb')
     print("File Created.")
     file.close()
 
 if (inp ==2):
-    name = input('Enter file name :')
-    file= open('data.dat',"a")
+   
+    file= open('data.DAT',"ab")
     aId = input("Enter application id : ")
     aName = input("Enter application name : ")
     aQual = input("Enter Qualification : ")
-
-    file.writelines("\n"+aId+","+aName+","+aQual)
+    
+    l = [aId,aName,aQual]
+    pickle.dump(l,file)
     file.close()
              
 if (inp ==3): 
-    name = input('Enter file name :')
-    file= open(name,"r")
+    
+    file= open('data.DAT',"r")
     inp = input("Input Qualification :")
-    l = []
     lfinal = []
-    for line in file:
-    
-        line = line.rstrip()
-        l=line.split(",")
-    
-        if(l[2]==inp):
-            lfinal.append(l[1])
-    print("Applicants with matching Qualifications :",lfinal)        
+    try:
+        while True:
+            appln=pickle.load(f)
+            if appln[2]==inp:
+                print(appln)
+                lfinal.append(appln)
+        print("Applicants with matching Qualifications :",lfinal)              
+    except EOFError :
+        f.close()
+         
 
         
 
 
 if (inp ==4):
-    name = input('Enter file name :')
-    fileOld= open(name,"r")
-    fileUpdated= open("updated"+name,"w")
-    inp = input("Input Application id to modify details :")
-    aName = input("Enter new application name  : ")
-    aQual = input("Enter new Qualification : ")
     
-   
-    for line in fileOld.readlines():
-
-        if not(line.startswith(inp)):
-            fileUpdated.write(line)
-    fileUpdated.write("\n"+inp+","+aName+","+aQual)
-    fileOld.close()
-    fileUpdated.close() 
+    app=[]
+	f=open('APPLICANT.DAT','rb+')
+	id=int(input('Enter id to search  '))
+	qual=input('Enter new qualification  ')
+	try:
+		while True:
+			rp=f.tell()
+			app=pickle.load(f)
+			if app[0]==id:
+				app[2]=qual
+				f.seek(rp)
+				pickle.dump(app,f)
+				break
+	except EOFError:
+		f.close()
 
 if(inp==5):
     print(">Quit")    
